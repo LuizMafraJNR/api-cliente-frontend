6735f7bcc425682524244516c939bdfe2ce44489
luiz.junior <luiz.junior@neomind.com.br>
2024-01-12 14:20:13 -0300

    Adicionando novas importações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PrincipalComponent],
+  imports: [CommonModule, RouterOutlet, PrincipalComponent, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-principal',
