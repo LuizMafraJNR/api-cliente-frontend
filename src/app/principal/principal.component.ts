8b1a2d03dc71832cc1bd49f97c420ff6bd448f39
luiz.junior <luiz.junior@neomind.com.br>
2024-01-12 13:58:16 -0300

    Alterando visibilidade do botão e importando CommonModule para ngIf

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
-
+  // Variavel para visibilidade dos botões
+  btnCadastro:Boolean = true;
 }
