c63ab0134a690406e47a2e70af03f14af475bb08
luiz.junior <luiz.junior@neomind.com.br>
2024-01-12 14:26:50 -0300

    Configurando nosso serviço

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ClienteService {
 
-  constructor() { }
+  // URL da API
+  private url:string = "hhtp://localhost:8080"
+
+  constructor(private http:HttpClient) {
+
+  }
 }
