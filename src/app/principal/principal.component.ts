a43596127a0295fb4251f30e23d5b8269664cd2d
luiz.junior <luiz.junior@neomind.com.br>
2024-01-16 20:41:10 -0300

    Configurado requisição para receber os cliente do nosso back-end

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { Cliente } from '../model/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -14,4 +16,16 @@ import { HttpClientModule} from '@angular/common/http';
 export class PrincipalComponent {
   // Variavel para visibilidade dos botões
   btnCadastro:Boolean = true;
+
+  // Vetor (Json) do tipo cliente
+  clientes:Cliente[] = [];
+
+  // Contrustor para service
+  constructor(private servico:ClienteService){}
+
+  // Método de seleção
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(response => this.clientes = response);
+  }
 }
--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../model/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,10 @@ export class ClienteService {
   // URL da API
   private url:string = "hhtp://localhost:8080"
 
-  constructor(private http:HttpClient) {
-
-  }
+  constructor(private http:HttpClient) {}
+    
+    // Método para selecionar todos os clientes
+    selecionar():Observable<Cliente[]>{
+      return this.http.get<Cliente[]>(this.url);
+    }
 }
