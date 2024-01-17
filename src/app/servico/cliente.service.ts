7000e8b51a84f4895869fc2c19c1db57f030fd1e
luiz.junior <luiz.junior@neomind.com.br>
2024-01-16 22:02:02 -0300

    Adicionado o método de cadastro de cliente

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -10,11 +10,17 @@ export class ClienteService {
 
   // URL da API
   private url:string = 'http://localhost:8080';
+  private urlCadastro:string = 'http://localhost:8080/cadastrar';
 
   constructor(private http:HttpClient) {}
     
     // Método para selecionar todos os clientes
     selecionar():Observable<Cliente[]>{
       return this.http.get<Cliente[]>(this.url);
+    }
+
+    // Método para cadastrar clientes
+    cadastrar(cliente:Cliente):Observable<Cliente>{
+      return this.http.post<Cliente>(this.urlCadastro, cliente);
     }
 }
